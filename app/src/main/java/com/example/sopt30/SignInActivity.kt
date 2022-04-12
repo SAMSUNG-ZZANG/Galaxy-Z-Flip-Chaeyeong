bb38e239f152f64d8e9370d8a86563cea822166f
chaeyeong <shimcy2010@gmail.com>
2022-04-12 15:25:23 +0900

    [REFACTOR] signin signup
    - 키워드 수정
    - val 수정

--- app/src/main/java/com/example/sopt30/SignInActivity.kt
+++ app/src/main/java/com/example/sopt30/SignInActivity.kt
@@ -1,12 +1,10 @@
 package com.example.sopt30
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.sopt30.databinding.ActivitySignInBinding
-import kotlin.math.sign
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding : ActivitySignInBinding
@@ -21,14 +19,14 @@ class SignInActivity : AppCompatActivity() {
         val signUpIntent = Intent(this, SignUpActivity::class.java)
 
         binding.btnLogin.setOnClickListener{
-            val et_id = binding.etId.getText().toString();
-            val et_pw = binding.etPw.getText().toString();
+            val etId = binding.etId.text.toString();
+            val etPw = binding.etPw.text.toString();
 
-            if(et_id.isEmpty() || et_pw.isEmpty()){
+            if(etId.isEmpty() || etPw.isEmpty()){
                 Toast.makeText(this, "로그인실패", Toast.LENGTH_SHORT).show();
             }
             else{
-                Toast.makeText(this, et_id.plus("님 환영합니다"), Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, etId.plus("님 환영합니다"), Toast.LENGTH_SHORT).show();
                 startActivity(homeIntent)
             }
         }
--- app/src/main/java/com/example/sopt30/SignUpActivity.kt
+++ app/src/main/java/com/example/sopt30/SignUpActivity.kt
@@ -9,17 +9,17 @@ import com.example.sopt30.databinding.ActivitySignUpBinding
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var binding: ActivitySignUpBinding = ActivitySignUpBinding.inflate(layoutInflater)
+        val binding: ActivitySignUpBinding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val signinIntent = Intent(this, SignInActivity::class.java)
 
         binding.btnSignUp.setOnClickListener {
-            val et_name = binding.etName.getText().toString();
-            val et_id = binding.etId.getText().toString();
-            val et_pw = binding.etPw.getText().toString();
+            val etName = binding.etName.getText().toString();
+            val etId = binding.etId.getText().toString();
+            val etPw = binding.etPw.getText().toString();
 
-            if(et_name.isEmpty() || et_id.isEmpty() || et_pw.isEmpty()) {
+            if(etName.isEmpty() || etId.isEmpty() || etPw.isEmpty()) {
                 Toast.makeText(this, "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show();
             }
             else {
