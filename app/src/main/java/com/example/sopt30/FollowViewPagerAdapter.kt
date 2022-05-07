1c265c263c11380d9b07fa48ae1ca500961518e6
chaeyeong <shimcy2010@gmail.com>
2022-05-07 13:35:24 +0900

    [FEAT] #4 - home fragment 탭 뷰 생성

---
+++ app/src/main/java/com/example/sopt30/FollowViewPagerAdapter.kt
@@ -0,0 +1,13 @@
+package com.example.sopt30
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class FollowViewPagerAdapter(fragment: Fragment) :
+    FragmentStateAdapter(fragment){
+        val fragments = mutableListOf<Fragment>()
+
+    override fun getItemCount(): Int = fragments.size
+
+    override fun createFragment(position: Int): Fragment = fragments[position]
+}
